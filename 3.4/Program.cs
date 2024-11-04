e34883bc5a8fc89bf66ac6c2c59f84adccfe1bb9
Екатерина Благочинная <e.blagochinnaya@dnevnik.ru>
2024-11-04 15:55:58 +0200

    Добавьте файлы проекта.

---
+++ 3.4/Program.cs
@@ -0,0 +1,26 @@
+﻿Console.WriteLine("┌-----------------------------------------------------------┐");
+Console.WriteLine($"|        x          |         u         |         f         |");
+Console.WriteLine("|-----------------------------------------------------------|");
+for (double x = 0; x <= 4; x += 0.4)
+{    
+    double f = 0;
+    double u = 0;
+    if (x <= 1.5)
+    {
+        u += Math.Pow(x, 2) - x + 1;
+        f = 1.5 + Math.Log(Math.Abs(Math.Sin(u)));
+    }
+    if (x > 1.5 && x <= 2.5)
+    {
+        u += Math.Atan(x);
+        f = 1.5 + Math.Log(Math.Abs(Math.Sin(u)));
+    }
+    else 
+    {
+        u += Math.Pow(Math.Sin(x - 1), 3) + Math.Cos(x + 1);
+        f = 1.5 + Math.Log(Math.Abs(Math.Sin(u)));
+    }
+    Console.WriteLine($"|       {x,-5:F2}       |       {u,5:F2}       |       {f,5:F2}       |");
+}
+Console.WriteLine("└-----------------------------------------------------------┘");
+Console.WriteLine();
